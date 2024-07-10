f4beb4df84ea7d0eeb772162884c5e722cb2899a
Dhruvil <dskathiriya1805@gmail.com>
2024-07-10 09:35:15 +0530

    First Exercise

---
+++ Exercises/Exercise 1.py
@@ -0,0 +1,15 @@
+# this is \\ Double backslash
+
+print("this is \\\\ Double backslash")
+
+# these are /\/\/\/\/\ mountains
+
+print("this are /\\/\\/\\/\\ mountains")
+
+# he is    awesome (Use escape sequence instead of manual spaces)
+
+print("he is \t awesome")
+
+# \" \n \t \' (print this as an output)
+
+print("\\\" \\n \\t \\' ")
