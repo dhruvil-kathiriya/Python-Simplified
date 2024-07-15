46afd1ceaa6ec071eb2582adac3508c3d7f69f03
Dhruvil <dskathiriya1805@gmail.com>
2024-07-15 09:59:47 +0530

    Third Commit

---
+++ Exercises/Exercise_6.py
@@ -0,0 +1,13 @@
+## Quadric Equation
+
+import math
+
+a = int(input('Enter a value:'))
+b = int(input('Enter b value:'))
+c = int(input('Enter c value:'))
+ 
+root1 = (-b + math.sqrt (b**2 -4 * a * c))/(2 * a)
+root2 = (-b - math.sqrt (b**2 -4 * a * c))/(2 * a)
+
+print('roots are ',root1,root2)
+
