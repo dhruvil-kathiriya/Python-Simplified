c4230e9476abddf221d05018a5375f6a79336ae2
Dhruvil <dskathiriya1805@gmail.com>
2024-07-15 09:51:14 +0530

    second Commit

---
+++ Exercises/Exercise_4.py
@@ -0,0 +1,42 @@
+## Find Rectangle Area
+
+l = int(input('Enter the Length of Rectangle'))
+b = int(input('Enter the Breath of Rectangle'))
+
+area = l * b
+
+print("Area of reactangle is ",area)
+
+
+## Find Triangle Area
+
+b = int(input('Enter the Breath of Triangle'))
+h = int(input('Enter the Height of Triangle'))
+
+area = 0.5 * b * h 
+
+print('Area of Triangle is ',area)
+
+
+## Find Rhombus area 
+
+a = int(input('Enter a value'))
+b = int(input('Enter b value'))
+h = int(input('Enter h value'))
+
+area = 0.5 * (a + b) * h
+
+print("Area is ",area)
+
+
+## Find Displacement Value
+
+v = int(input('Enter Initial Velocity'))
+u = int(input('Enter Final Velocity'))
+a = int(input('Enter Acceleration'))
+
+d = ( v**2 - u**2 ) / (2 * a)
+
+print('Displacement is ',d)
+
+
---
+++ Exercises/Exercise_5.py
@@ -0,0 +1,16 @@
+## Convert km to miles
+
+km = int(input("Enter Kelometer value"))
+
+mile = km * 0.621371  
+print("Your Miles are ",mile)
+
+
+## Find area of Circle
+import math
+
+r = float(input('Enter Radious of circle'))
+
+area = math.pi * r * r
+
+print('Area of Circle is',area)
